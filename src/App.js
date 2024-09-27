9825bc5b0ef9ec993540575ef4b353e0baf970b0
BilalK <test.dev427@gmail.com>
2024-09-27 12:00:54 +0500

    Load More

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Accordian from "./components/accordian/Accordian";
 import ImageSlider from "./components/imageSlider/ImageSlider";
+import LoadMore from "./components/loadmore/LoadMore";
 import RandomColor from "./components/randomColor/RandomColor";
 import StarRating from "./components/starRating/StarRating";
 
 function App() {
   return (
     <>
+      <LoadMore />
       <ImageSlider />
       <StarRating stars={5} />
       <RandomColor />
